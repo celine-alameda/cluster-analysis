548655c68a929e594d8a40f8be549d64a8d797ea
Céline Alameda <52200174+celine-alameda@users.noreply.github.com>
2021-10-10 11:59:05 +0200

    separate tfce file

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import math
+
 import pandas as pd
 from scipy import stats
 from tqdm import tqdm
-
+from tfce_computation import tfce_from_distribution
 # values as per doi:10.1016/j.neuroimage.2008.03.061
+
+
 extend_weight = 0.5
 height_weight = 2
 dh = 0.1
@@ -20,7 +22,6 @@ data_frame = pd.read_csv('data/data.tsv', sep="\t")
 datapoint_list = data_frame.loc[data_frame[trial_name] == 1, datapoint_name].unique().tolist()
 
 t_values = []
-
 # compute t value for each datapoint to establish clusters
 for datapoint in tqdm(datapoint_list):
     values_pre = data_frame.loc[
@@ -32,46 +33,12 @@ for datapoint in tqdm(datapoint_list):
     t, p = stats.ttest_rel(values_post, values_pre)
     t_values.append(t)
 
-# TFCE enhancement
-tfce_values = []
-
-for i in tqdm(range(len(t_values))):
-    # floor to 0.1
-    # notations are similar to those in the paper
-    if t_values[i] == 0:
-        tfce_values.append(0)
-        continue
-    signum = t_values[i] / abs(t_values[i])
-    h_p = math.floor(abs(t_values[i]) / dh) * dh
-    height = dh
-    tfce = 0
-    while height <= h_p:
-        # extent is how many samples have values of at least h
-        # reach forward
-        extend = 1  # at least this sample
-        index = i + 1
-        while index < len(t_values):
-            if abs(t_values[index] < height):
-                break
-            extend += 1
-            index += 1
-        # reach backward
-        index = i - 1
-        while index > 0:
-            if abs(t_values[index] < height):
-                break
-            extend += 1
-            index += 1
-        tfce = tfce + math.pow(extend, extend_weight) * math.pow(height, height_weight)
-        height += dh
-    tfce = tfce * signum
-    tfce_values.append(tfce)
-
 # for plotting in R
 t_data = pd.DataFrame(t_values)
 t_data = t_data.rename(columns={0: "t"})
 t_data.to_csv("outputs/t_values.tsv", header=True, index=False)
 
+tfce_values = tfce_from_distribution(t_values)
 tfce_data = pd.DataFrame(tfce_values)
 tfce_data = tfce_data.rename(columns={0: "tfce"})
 tfce_data.to_csv("outputs/tfce_values.tsv", header=True, index=False)
---
+++ tfce_computation.py
@@ -0,0 +1,39 @@
+import math
+dh = 0.1
+
+def tfce_from_distribution(distribution: list):
+    """Given a distribution (1D list of values), computes the Threshold-Free Cluster Enhancement"""
+
+    tfce_values = []
+    for i in range(len(distribution)):
+        # floor to 0.1
+        # notations are similar to those in the paper
+        if distribution[i] == 0:
+            tfce_values.append(0)
+            continue
+        signum = distribution[i] / abs(distribution[i])
+        h_p = math.floor(abs(distribution[i]) / dh) * dh
+        height = dh
+        tfce = 0
+        while height <= h_p:
+            # extent is how many samples have values of at least h
+            # reach forward
+            extend = 1  # at least this sample
+            index = i + 1
+            while index < len(distribution):
+                if abs(distribution[index] < height):
+                    break
+                extend += 1
+                index += 1
+            # reach backward
+            index = i - 1
+            while index > 0:
+                if abs(distribution[index] < height):
+                    break
+                extend += 1
+                index += 1
+            tfce = tfce + math.pow(extend, extend_weight) * math.pow(height, height_weight)
+            height += dh
+        tfce = tfce * signum
+        tfce_values.append(tfce)
+    return tfce_values
