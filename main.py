281d646ff6b072e4a2e8937778bf6a89ff34855b
Céline Alameda <alamedaceline@gmail.com>
2021-11-09 09:50:05 +0100

    added possibility for no bootstrap

--- main.py
+++ main.py
@@ -18,9 +18,10 @@ if __name__ == "__main__":
     seed = 42
     rng = np.random.default_rng(42)
     datapoint_list = []
+    bootstrap = False
 
     inputFiles = []
-    analyses = ["3_f", "3_u"]
+    analyses = ["4_fa", "4_ua"]
     subjects = list(range(1, 6)) + list(range(7, 16))
     conditions_tdcs = ["real", "sham"]
     conditions_time = ["pre", "post"]
@@ -48,39 +49,40 @@ if __name__ == "__main__":
         tfce_data = pd.DataFrame(actual_tfce_list)
         tfce_data = tfce_data.rename(columns={0: "tfce"})
 
-        # now, resample and check for significance
-        max_tfces = []
-        min_tfces = []
+        if bootstrap:
+            # now, resample and check for significance
+            max_tfces = []
+            min_tfces = []
 
-        all_resamplings = []
-        print("Generating {} resamplings".format(n_resamplings))
-        for _ in range(n_resamplings):
-            if tb.is_condition:
-                all_resamplings.append(tb.shuffle_t_cluster_sign(actual_t_list))
-            else:
-                all_resamplings.append(tb.shuffle_t_cluster_position(actual_t_list))
-        print("Processing resamplings with {} processes".format(n_workers))
-        with concurrent.futures.ProcessPoolExecutor(max_workers=n_workers) as executor:
-            futures = {executor.submit(compute_resampling,
-                                       all_resamplings,
-                                       i * n_resamplings // n_workers,
-                                       n_resamplings // n_workers): i for i
-                       in range(n_workers)}
-            for future in concurrent.futures.as_completed(futures):
-                min_tfce, max_tfce = future.result()
-                min_tfces.append(min_tfce)
-                max_tfces.append(max_tfce)
+            all_resamplings = []
+            print("Generating {} resamplings".format(n_resamplings))
+            for _ in range(n_resamplings):
+                if tb.is_condition:
+                    all_resamplings.append(tb.shuffle_t_cluster_sign(actual_t_list))
+                else:
+                    all_resamplings.append(tb.shuffle_t_cluster_position(actual_t_list))
+            print("Processing resamplings with {} processes".format(n_workers))
+            with concurrent.futures.ProcessPoolExecutor(max_workers=n_workers) as executor:
+                futures = {executor.submit(compute_resampling,
+                                           all_resamplings,
+                                           i * n_resamplings // n_workers,
+                                           n_resamplings // n_workers): i for i
+                           in range(n_workers)}
+                for future in concurrent.futures.as_completed(futures):
+                    min_tfce, max_tfce = future.result()
+                    min_tfces.append(min_tfce)
+                    max_tfces.append(max_tfce)
 
-        lower = np.percentile(min_tfces, 100 * alpha / 2.0)
-        upper = np.percentile(max_tfces, 100 * (1 - (alpha / 2.0)))
-        significance = []
-        for i in range(len(actual_tfce_list)):
-            actual_tfce = actual_tfce_list[i]
-            if lower <= actual_tfce <= upper:
-                significance.append(0)
-            else:
-                significance.append(1)
+            lower = np.percentile(min_tfces, 100 * alpha / 2.0)
+            upper = np.percentile(max_tfces, 100 * (1 - (alpha / 2.0)))
+            significance = []
+            for i in range(len(actual_tfce_list)):
+                actual_tfce = actual_tfce_list[i]
+                if lower <= actual_tfce <= upper:
+                    significance.append(0)
+                else:
+                    significance.append(1)
+            tfce_data["sig"] = significance
 
-        tfce_data["sig"] = significance
         tfce_data.to_csv("outputs/tfce_" + data_file.split('.')[0] + ".tsv", header=True, index=False, sep='\t')
         print("done " + data_file)
